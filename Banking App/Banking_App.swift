9c4b8df05e22742ad9dcd416285f42be45f7c572
dontdoitno <v.p.persuk@gmail.com>
2024-03-18 01:21:20 +0300

    Initial commit

---
+++ Banking App/Banking_App.swift
@@ -0,0 +1,17 @@
+//
+//  Banking_AppApp.swift
+//  Banking App
+//
+//  Created by Виктория on 17.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Banking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
