b7437ebfd35798a569b4f589d7674c77d5c7a652
dontdoitno <v.p.persuk@gmail.com>
2024-03-21 18:17:23 +0300

    добавлены заглушки на страницы

---
+++ Views/MessagePage/MessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageView.swift
+//  Banking App
+//
+//  Created by Виктория on 21.03.2024.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text("There will be Message View")
+    }
+}
+
+#Preview {
+    MessageView()
+}
---
+++ Views/SearchPage/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  Banking App
+//
+//  Created by Виктория on 21.03.2024.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("There will be Search View")
+    }
+}
+
+#Preview {
+    SearchView()
+}
